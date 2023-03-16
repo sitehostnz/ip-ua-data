fdd837eddee24102b32a19302f3b09251780b1d1
Eric Burgueño <eric.burgueno@sitehost.co.nz>
2023-03-16 14:36:14 +1300

    - Updated script to generate list of public cloud IPs using a single larger data source that includes many hosting providers - Update gen-geo-ips.sh to use a more descriptive table name in the database

--- 3-gen-public-cloud-ranges.sh
+++ 3-gen-public-cloud-ranges.sh
@@ -1,15 +1,48 @@
 #!/bin/bash
-CLOUD_IPS_DIR="public-cloud-ips"
-mkdir -p "$CLOUD_IPS_DIR"
+HOSTING_IPS_DIR="public-cloud-ips"
+rm -rf "$HOSTING_IPS_DIR"
+mkdir -p "$HOSTING_IPS_DIR"
 
-#Azure Cloud
-wget -q -O - https://azureipranges.azurewebsites.net/Data/Public.json | jq -r '.values[] | select(.name == "AzureCloud") | .properties.addressPrefixes[]' > "$CLOUD_IPS_DIR/azure-cloud"
+CONTAINER=$(docker ps -q)
 
-#AWS EC2
-wget -q -O - https://ip-ranges.amazonaws.com/ip-ranges.json | grep 'EC2"' -B2 |grep -E 'ip_prefix|ipv6_prefix'|awk '{print $NF}'|sed -e 's/\"//g' -e 's/,//' > "$CLOUD_IPS_DIR/aws-ec2"
+wget -q -O /tmp/hostingRanges.tsv.zip https://github.com/NikolaiT/IP-Address-API/raw/main/databases/hostingRanges.tsv.zip
+cd /tmp
+unzip hostingRanges.tsv.zip
+cd -
 
-#CGP
-wget -q -O - https://www.gstatic.com/ipranges/cloud.json | jq -r '(.prefixes[] | select(.ipv4Prefix != null) | .ipv4Prefix), (.prefixes[] | select(.ipv6Prefix != null) | .ipv6Prefix)' > "$CLOUD_IPS_DIR/google-cloud-platform"
+#preprocessing
+##if last field is empty, use a trimmed version of the first one
+##if range is cidr already, add empty start and end fields
+##else, add empty cidr field
+##remove first field
+cat /tmp/hostingRanges.tsv | awk -F'\t' 'BEGIN{OFS="\t"} {if ($NF=="") { gsub(/[^[:alnum:]]/, "", $1); $NF=$1 }} { gsub(/[[:space:]]+/,"",$2) } {if ($2 ~ /\//) { $2="\t\t"$2 } else { gsub(/-/,"\t",$2); $3="\t"$3 }} {for(i=2;i<=NF-1;i++) printf $i"\t"; print $NF}' > /tmp/preprocessed.tsv
 
-#Digital Ocean
-wget -q -O - https://www.digitalocean.com/geo/google.csv|awk -F, '{print $1}' > "$CLOUD_IPS_DIR/digital-ocean"
+docker cp /tmp/preprocessed.tsv "${CONTAINER}":/tmp/preprocessed.tsv
+docker exec -u postgres "${CONTAINER}" mkdir /tmp/pgexport
+
+export PGPASSWORD="postgres"
+psql -h localhost -p 5432 -U postgres ipdata -c "CREATE TABLE hosting(ipstart INET,ipend INET, iprange CIDR, provider TEXT NOT NULL);"
+
+psql -h localhost -p 5432 -U postgres ipdata -c "COPY hosting FROM '/tmp/preprocessed.tsv' WITH (DELIMITER E'\t', NULL '');"
+
+psql -h localhost -p 5432 -U postgres ipdata -c "UPDATE hosting SET iprange=inet_merge(ipstart, ipend) WHERE iprange IS NULL;"
+psql -h localhost -p 5432 -U postgres ipdata <<'EOF'
+DO $$
+DECLARE
+rec record;
+filename text;
+BEGIN
+FOR rec IN SELECT DISTINCT provider FROM hosting LOOP
+filename := '/tmp/pgexport/' || rec.provider;
+EXECUTE format('COPY (SELECT iprange as cidr FROM hosting WHERE provider = %L ORDER BY cidr) TO %L', rec.provider, filename);
+END LOOP;
+END $$;
+EOF
+
+docker exec "${CONTAINER}" tar -cf /tmp/pgexport.tar /tmp/pgexport
+docker cp "${CONTAINER}":/tmp/pgexport.tar "$HOSTING_IPS_DIR"/pgexport.tar
+cd "$HOSTING_IPS_DIR"
+tar --strip-components=2 -xf pgexport.tar
+rm -f pgexport.tar
+cd
+
--- 6-get-geo-ranges.sh
+++ 6-get-geo-ranges.sh
@@ -13,12 +13,12 @@ docker cp /tmp/ip2country-v6.tsv "${CONTAINER}":/tmp/ip2country-v6.tsv
 docker exec -u postgres "${CONTAINER}" mkdir /tmp/pgexport
 
 export PGPASSWORD="postgres"
-psql -h localhost -p 5432 -U postgres ipdata -c "CREATE TABLE iptable(ipstart INET NOT NULL,ipend INET NOT NULL, country TEXT NOT NULL);"
+psql -h localhost -p 5432 -U postgres ipdata -c "CREATE TABLE geoips(ipstart INET NOT NULL,ipend INET NOT NULL, country TEXT NOT NULL);"
 
-psql -h localhost -p 5432 -U postgres ipdata -c "COPY iptable FROM '/tmp/ip2country-v4.tsv' DELIMITER E'\t';"
-psql -h localhost -p 5432 -U postgres ipdata -c "COPY iptable FROM '/tmp/ip2country-v6.tsv' DELIMITER E'\t';"
+psql -h localhost -p 5432 -U postgres ipdata -c "COPY geoips FROM '/tmp/ip2country-v4.tsv' DELIMITER E'\t';"
+psql -h localhost -p 5432 -U postgres ipdata -c "COPY geoips FROM '/tmp/ip2country-v6.tsv' DELIMITER E'\t';"
 #special case for when country is empty
-psql -h localhost -p 5432 -U postgres ipdata -c "UPDATE iptable SET country='Unknown' WHERE country = '';"
+psql -h localhost -p 5432 -U postgres ipdata -c "UPDATE geoips SET country='Unknown' WHERE country = '';"
 
 psql -h localhost -p 5432 -U postgres ipdata <<'EOF'
 DO $$
@@ -26,9 +26,9 @@ DECLARE
 rec record;
 filename text;
 BEGIN
-FOR rec IN SELECT DISTINCT country FROM iptable LOOP
+FOR rec IN SELECT DISTINCT country FROM geoips LOOP
 filename := '/tmp/pgexport/' || rec.country;
-EXECUTE format('COPY (SELECT inet_merge(ipstart,ipend) AS cidr FROM iptable WHERE country = %L ORDER BY cidr) TO %L', rec.country, filename);
+EXECUTE format('COPY (SELECT inet_merge(ipstart,ipend) AS cidr FROM geoips WHERE country = %L ORDER BY cidr) TO %L', rec.country, filename);
 END LOOP;
 END $$;
 EOF
